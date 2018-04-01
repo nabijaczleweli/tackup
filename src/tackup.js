9371aedb697de7ed9a98905f0f6ef8e766917a14
nabijaczleweli <nabijaczleweli@gmail.com>
2018-04-01 07:52:32 +0200

    Save tabs to localstorage on a timer

--- src/tackup.js
+++ src/tackup.js
@@ -1,7 +1,53 @@
-browser.tabs.query({}).then(tabs => {
-	console.log(tabs.length);
+// The MIT License (MIT)
 
-	tabs.forEach((tab, i) => {
-		console.log(i, tab.title, tab.url);
+// Copyright (c) 2018 nabijaczleweli
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+const DEFAULT_CONFIG = {
+	interval: 10 * 60 * 1000
+};
+
+
+function setup_interval(config) {
+	let setup = config => {
+		if(typeof config !== "object" || typeof config.interval !== "number")
+			config = DEFAULT_CONFIG;
+
+		console.log("[tackup]", "Saving again in", config.interval, "ms");
+		setTimeout(tab_event_callback, config.interval);
+	};
+
+	browser.storage.local.get("config").then(setup, setup);
+}
+
+function tab_event_callback() {
+	let now = (new Date()).toISOString();
+
+	browser.tabs.query({}).then(tabs => {
+		browser.storage.local.set({tabs: {time: now, tabs: tabs.map(tab => ({title: tab.title, url: tab.url, private: tab.incognito}))}})
+		    .then(()     => console.log("[tackup]", now, "Successfully saved", tabs.length, "tabs"),
+		          reason => console.log("[tackup]", now, "Failed to save", tabs.length, "tabs:", reason));
+
+		setup_interval();
 	});
-});
+}
+
+
+tab_event_callback();
