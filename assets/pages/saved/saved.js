nabijaczleweli/tackup

commit ef2de92452ca8cc6d9412eb0308dc20ddc101009

Save windowId. saved: add ability to [un]select by window
https://cohost.org/nabijaczleweli/post/1217501-got-tackup-hate-mail

assets/pages/saved/saved.js
@@ -28,6 +28,7 @@ window.addEventListener("load", () => {
 	const TIMESTAMP_SELECT        = document.getElementsByName("timestamp-select")[0];
 	const LOCAL_DATETIMES         = document.getElementById("local-datetimes");
 	const OPEN_SELECTED           = document.getElementById("open-selected");
+	const WINDOWS                 = document.getElementById("windows");
 
 	browser.storage.local.get(null).then(data => {
 		delete data.config;
@@ -44,20 +45,39 @@ window.addEventListener("load", () => {
 
 		TABSET_TABLE.hidden     = false;
 		let tabset_table_header = TABSET_TABLE.innerHTML;
+		let windows             = [];
 
 		TIMESTAMP_SELECT.addEventListener("change", () => {
 			if(data[TIMESTAMP_SELECT.value] !== undefined) {
 				let quote_escaped      = encodeURIComponent('"');
 				TABSET_TABLE.innerHTML = data[TIMESTAMP_SELECT.value].reduce(
-				    (acc, val) => acc + `<tr><td ${val.private ? "class=\"private\"" : ""}></td> <td><a href="${val.url.replace("\"", quote_escaped)}">${
-							                      val.title.replace("<", "&lt;").replace(">", "&gt;")}</a></td> <td><input type="checkbox" class="select-box"></input></td></tr>\n`,
+				    (acc, val) => acc + `<tr><td ${val.private ? "class=\"private\"" : ""}></td> <td>${("window" in val) ? val.window : ""}</td> <td><a href="${
+							                      val.url.replace("\"", quote_escaped)}">${
+								                    val.title.replace("<", "&lt;").replace(">", "&gt;")}</a></td> <td><input type="checkbox" class="select-box" ${
+									                  ("window" in val) ? `window=${val.window}` : ""}></input></td></tr>\n`,
 						tabset_table_header);
+				windows           = Array.from(data[TIMESTAMP_SELECT.value].reduce((acc, val) => {
+					          if("window" in val)
+            acc.add(val.window);
+          return acc;
+				          }, new Set()));
+				WINDOWS.innerHTML = windows.reduce((acc, val) => acc + `<option value=${val}>`, "");
 
-				document.getElementById("main-select-box").addEventListener("change", ev => {
-					const MAIN_SELECT_BOX = ev.target;
+				document.getElementById("all-select-box").addEventListener("change", ev => {
+					const ALL_SELECT_BOX = ev.target;
 
-					Array.from(document.getElementsByClassName("select-box")).forEach(select_box => select_box.checked = MAIN_SELECT_BOX.checked);
+					Array.from(document.getElementsByClassName("select-box")).forEach(select_box => select_box.checked = ALL_SELECT_BOX.checked);
 				});
+
+				const WINDOW_SELECT_BOX       = document.getElementById("window-select-box");
+				const WINDOW_SELECT_NUMBER    = document.getElementById("window-select-number");
+				WINDOW_SELECT_NUMBER.disabled = WINDOW_SELECT_BOX.disabled = !windows.length
+				if(WINDOW_SELECT_NUMBER.disabled)
+				WINDOW_SELECT_NUMBER.placeholder = "∅";
+
+				WINDOW_SELECT_BOX.addEventListener(
+				    "change", ev => Array.from(document.querySelectorAll(`.select-box[window="${WINDOW_SELECT_NUMBER.value.replace("\"", quote_escaped)}"]`))
+					                      .forEach(select_box => select_box.checked = WINDOW_SELECT_BOX.checked));
 			}
 		});
 		TIMESTAMP_SELECT.dispatchEvent(new CustomEvent("change", {}));
@@ -73,10 +93,12 @@ window.addEventListener("load", () => {
 				if(select_box.checked) {
 					let url = select_box.parentElement.parentElement.children[1].firstElementChild.href;
 
-					browser.tabs.create({
-						active: false,
-						url: url,
-					}).then(() => {}, err => console.log("Couldn't open tab", url, "due to", err));
+					browser.tabs
+					    .create({
+						    active: false,
+						    url: url,
+					    })
+					    .then(() => {}, err => console.log("Couldn't open tab", url, "due to", err));
 				}
 			});
 		});

assets/pages/saved/index.html
@@ -20,8 +20,13 @@
 		</span>
 
 		<table id="tabset-table" hidden>
-			<tr><th><span style="opacity: 0;">@</span></th> <th>Title &amp; URL</th> <th><input type="checkbox" id="main-select-box"></input></th></tr>
+			<tr><th><span style="opacity: 0;">@</span></th>
+			    <th class="window-icon"><span style="opacity: 0;">🗔</span></th>
+			    <th>Title &amp; URL</th>
+			    <th><label style="line-height: 0; /* Otherwise this row gets super tall */">∀ <input type="checkbox" id="all-select-box"></input></label></th>
+			    <th><label><span class="window-icon"><span style="opacity: 0;">🗔</span></span> <input id="window-select-number" type="text" list="windows" placeholder="↓" style="width: 2em;" disabled></label> <input type="checkbox" id="window-select-box" disabled></input></th></tr>
 		</table>
+		<datalist id="windows"></datalist>
 
 		<p>
 			<label>Display timestamps in local timezone <input type="checkbox" id="local-datetimes" /></label><br />

assets/pages/saved/saved.css
@@ -2,6 +2,13 @@ th {
 	text-align: left;
 }
 
+.window-icon {
+	background-image: url("chrome://browser/skin/window.svg");
+	background-position: center;
+	background-size: 1em;
+	background-repeat: no-repeat;
+}
+
 td.private {
 	background-image: url("chrome://browser/skin/privatebrowsing/private-browsing.svg");
 	background-position: center;

src/tackup.js
@@ -97,7 +97,7 @@ function tab_event_callback() {
 
 	browser.tabs.query({}).then(tabs => {
 		let data      = {};
-		data[now]     = tabs.map(tab => ({title: tab.title, url: tab.url, private: tab.incognito}));
+		data[now]     = tabs.map(tab => ({title: tab.title, url: tab.url, private: tab.incognito, window: tab.windowId}));
 		data.freshest = now;
 
 		let maybe_save = cf => {

commit 765cc32ab200c7ca7ec0ae999c02f0063ffe0676

Fix opening selected tabs
Fixes: commit ef2de92452ca8cc6d9412eb0308dc20ddc101009 ("Save windowId.
 saved: add ability to [un]select by window")

assets/pages/saved/saved.js
@@ -91,7 +91,7 @@ window.addEventListener("load", () => {
 		OPEN_SELECTED.addEventListener("click", () => {
 			Array.from(document.getElementsByClassName("select-box")).forEach(select_box => {
 				if(select_box.checked) {
-					let url = select_box.parentElement.parentElement.children[1].firstElementChild.href;
+					let url = select_box.parentElement.parentElement.children[2].firstElementChild.href;
 
 					browser.tabs
 					    .create({